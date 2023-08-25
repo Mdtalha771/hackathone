4907087ed0eea48647677c435d38a2393b8d3f9f
Muhammad Talha <mdtalha771@gmail.com>
2023-08-25 18:02:20 +0500

    hackathone

---
+++ login.js
@@ -0,0 +1,82 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDB1vm9NMtfpTL7ljTqHpP6qRxKSzBL1mk",
+    authDomain: "blogging-app-e6f25.firebaseapp.com",
+    projectId: "blogging-app-e6f25",
+    storageBucket: "blogging-app-e6f25.appspot.com",
+    messagingSenderId: "805560499183",
+    appId: "1:805560499183:web:36905f1f9a7aba961b141a",
+    measurementId: "G-PXEH3G64FZ"
+  };
+  
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+// show Password
+function showPassword(event) {
+    event.target.className = "eye bi bi-eye-slash";
+    event.target.previousElementSibling.type = "text";
+    event.target.removeEventListener('click', showPassword);
+    event.target.addEventListener('click', hidePassword);
+}
+
+// hide password
+function hidePassword(event) {
+    event.target.className = "eye bi bi-eye";
+    event.target.previousElementSibling.type = "password";
+    event.target.removeEventListener('click', hidePassword);
+    event.target.addEventListener('click', showPassword);
+}
+
+function login(event) {
+    event.preventDefault()
+    let email = document.getElementById("email-login").value
+    let password = document.getElementById("password-login").value
+    let message = document.querySelector(".validationMessage");
+
+    if (!(email.endsWith("@gmail.com"))) {
+        message.innerText = `Invalid email address`;
+        message.style.display = "block";
+        message.style.color = "#e55865";
+        return;
+    }
+
+    if (
+        email.trim() === '' ||
+        password.trim() === ''
+        // || password.length > 8 || password.length < 4
+    ) {
+        message.innerText = `Please fill required fields`;
+        message.style.display = "block";
+        message.style.color = "#e55865";
+        return;
+    }
+
+    // firebase
+
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // console.log("Login successful");
+            Swal.fire({
+                icon: 'success',
+                title: 'Logged In',
+                text: 'Login Successfull',
+                confirmButtonColor: "#0079ff"
+            })
+            window.location.href = "./index.html";
+        })
+        .catch((error) => {
+            console.log("Login error:", error);
+            Swal.fire({
+                    icon: 'error',
+                    title: 'Access Denied',
+                    text: 'Invalid email or password. Please enter correct credentials',
+                    confirmButtonColor: "#0079ff"
+                })
+                // alert("Invalid email or password. Please enter correct credentials.");
+        });
+
+    document.getElementById("email-login").value
+    document.getElementById("password-login").value
+}
